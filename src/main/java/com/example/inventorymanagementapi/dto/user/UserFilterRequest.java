822b7a0837a501ecf7a6e55af29c7829abf79ffc
Kanan <kananazera@gmail.com>
2025-07-27 22:58:49 +0400

    update backend

--- src/main/java/com/example/inventorymanagementapi/dto/user/UserFilterRequest.java
+++ src/main/java/com/example/inventorymanagementapi/dto/user/UserFilterRequest.java
@@ -1,11 +1,9 @@
 package com.example.inventorymanagementapi.dto.user;
 
-import com.example.inventorymanagementapi.dto.productcategory.ProductCategoryFilterRequest;
 import com.example.inventorymanagementapi.enums.Gender;
 import lombok.Builder;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
