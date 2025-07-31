13bcfe2d9555ddcdc93824c4231f7588c0130ea5
Kanan <kananazera@gmail.com>
2025-08-01 00:25:41 +0400

    remove password field from user response dto

--- src/main/java/com/example/inventorymanagementapi/dto/user/UserResponse.java
+++ src/main/java/com/example/inventorymanagementapi/dto/user/UserResponse.java
@@ -13,7 +13,6 @@ public class UserResponse {
 
     private Long id;
     private String username;
-    private String password;
     private String email;
     private String fullName;
     private String phone;
